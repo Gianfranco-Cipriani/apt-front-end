54152a8b60932f915e24f44b5896d904efb38a6b
gAzraelc <68873524+gAzraelc@users.noreply.github.com>
2021-06-03 21:15:45 +0200

    Setup
    This file contains a setup to automate the making of an alias and makes executable the main script.

---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+# Copyright (C) 2020 Gianfranco Cipriani
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+#
+# Contact me on gianfrancocipriani@hotmail.com
+
+
+############################################################
+# WARNING: make sure to execute this script as SUDO
+# Leave files paths untouched otherwise modify this script
+# to work properly.
+############################################################
+
+# Install prerequisite packages
+sudo apt install dialog
+echo "install required packages done"
+
+# Check for shell type and make an alias
+echo make an alias...
+if [ $SHELL == "/usr/bin/bash" || $SHELL == "/bin/bash" ]
+then
+    sudo printf "%s\n" "alias sys-maintenance=~/apt-front-end/apt-front-end-1.0.0.sh" >> ~/.bashrc
+    echo "alias sys-maintenance set"
+elif [ $SHELL == "/bin/zsh" || $SHELL == "/usr/bin/zsh"]
+then
+    sudo printf "%s\n" "alias sys-maintenance=~/apt-front-end/apt-front-end-1.0.0.sh" >> ~/.zshrc
+    echo "alias sys-maintenance set"
+
+echo Done
